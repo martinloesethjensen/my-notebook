cfba0242a7dbcf0058c406855d8a721a2f7b59d3
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-03-22 21:58:51 +0100

    Initial Commit

---
+++ My Notebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Notebook
+//
+//  Created by Martin Løseth Jensen on 22/03/2019.
+//  Copyright © 2019 Martin Løseth Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
